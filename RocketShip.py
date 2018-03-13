77097f93a0fa681ff7a2cd679e8eeb383e714f83
ZackW9909 <36928624+zackW9902@users.noreply.github.com>
2018-03-13 10:47:44 -0500

    Update RocketShip.py

--- RocketShip.py
+++ RocketShip.py
@@ -2,7 +2,6 @@ from Graphics import *
 
 
 
-
 class Rocket(object):
     def __init__(self, x, y, color, win):
         self.x = x
@@ -10,28 +9,44 @@ class Rocket(object):
         self.color = color
         self.buildBooster(x,y, color, win)
 
+    def buildBooster(self,x,y, color, win):
+        body = Rectangle(Point(x,y), Point(x +50, y +300))
+        body.setFill(color)
+        engin = Polygon(Point(x,y +275),Point(x +50,y+275),Point(x +62.5,y +300),Point(x -25,y +100))
+        engin.setFill("black")
+        cap = Polygon(Point(x, y), Point(x + 25, y - 25), Point(x + 50, y))
+        cap.setFill(color)
+        fire = Image("flame.gif",Point(350, 575))
+        fire.draw(win)
+        body.draw(win)
+        engin.draw(win)
+        cap.draw(win)
+
+
+
+
 
 class Capsule(object):
-    def __init__(self):()
-    window = Circle(Point(475,200),10.5)
-    cap = Polygon(Point(550,250),Point(550,200),Point(500,150),Point(450,150),Point(400,200),Point(400,250))
-    cap.setFill()
-    cap.setOutline("black")
-    cap.draw(win)
+    def __init__(self, win):
+        window = Circle(Point(475, 200), 10.5)
+        cap = Polygon(Point(550, 250), Point(550, 200), Point(500, 150), Point(450, 150), Point(400, 200),Point(400, 250))
+        cap.setFill('orange')
+        cap.draw(win)
+        body = Rectangle(Point(400, 250), Point(550, 525))
+        body.draw(win)
+        eng1 = Rectangle(Point(400, 525), Point(550, 575))
+        eng2 = Rectangle(Point(425, 575), Point(525, 600))
+        eng1.draw(win)
+        eng2.draw(win)
 
-
-
-class Booster(object):
-    def __init__(self):
-        boosters = Rectangle(Point(275,300),Point(375,550))
-        boosters.setFill ("red")
-        boosters.setOutline ("black")
-
+        MrB = Image("Mr.B!.jpg",Point(475, 200))
+        MrB.draw(win)
 
 
 
 class RocketShip(object):
-    def __init__(self):
-
-
+    def __init__(self,win):
+        cap = Capsule
+        boost = Rocket(575,275,"red",win)
+        boost = Rocket(325,275, "red", win)
 
