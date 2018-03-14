5c6b432e3bd7bc0bb28232515dd25e7b7de5a4af
ZackW9909 <36928624+zackW9902@users.noreply.github.com>
2018-03-14 10:44:21 -0500

    Update RocketShip.py

--- RocketShip.py
+++ RocketShip.py
@@ -2,6 +2,7 @@ from Graphics import *
 
 
 
+
 class Rocket(object):
     def __init__(self, x, y, color, win):
         self.x = x
@@ -9,44 +10,65 @@ class Rocket(object):
         self.color = color
         self.buildBooster(x,y, color, win)
 
-    def buildBooster(self,x,y, color, win):
-        body = Rectangle(Point(x,y), Point(x +50, y +300))
+    def buildBooster(self, x, y, color, win):
+        body = Rectangle(Point(x,y),Point(x +55, y +275))
         body.setFill(color)
-        engin = Polygon(Point(x,y +275),Point(x +50,y+275),Point(x +62.5,y +300),Point(x -25,y +100))
-        engin.setFill("black")
-        cap = Polygon(Point(x, y), Point(x + 25, y - 25), Point(x + 50, y))
+        engin = Polygon(Point(x,y +275),Point(x +55,y +275),Point(x +75,y +300),Point(x -25,y + 300))
+        engin.setFill(color)
+        cap = Polygon(Point(x,y),Point(x +25,y -25),Point(x +55,y))
         cap.setFill(color)
-        fire = Image("flame.gif",Point(350, 575))
-        fire.draw(win)
+        fire = Image(Point(330, 625),"flame.gif")
+        fire2 = Image(Point(600, 625), "flame.gif")
         body.draw(win)
         engin.draw(win)
         cap.draw(win)
+        fire.draw(win)
+        fire2.draw(win)
 
 
 
 
 
 class Capsule(object):
-    def __init__(self, win):
-        window = Circle(Point(475, 200), 10.5)
-        cap = Polygon(Point(550, 250), Point(550, 200), Point(500, 150), Point(450, 150), Point(400, 200),Point(400, 250))
-        cap.setFill('orange')
+    def __init__(self,x,y,color,win):
+        self.drawBody(win)
+        self.drawEng(win)
+        self.drawTop(win,x,y,color)
+    def drawTop(self, win,x,y,color):
+        cap = Polygon(Point(x,y),Point(x,y - 50),Point(x + 50,y - 100),Point(x + 100,y - 100),Point(x + 150,y - 50),Point(x + 150,y))
+        cap.setFill(color)
         cap.draw(win)
-        body = Rectangle(Point(400, 250), Point(550, 525))
+        #MrB = Image(Point(475, 200),"Mr.B!.jpg")
+        #MrB.draw(win)
+        pole = Rectangle(Point(450,50), Point(500,150))
+        pole.draw(win)
+        tri = Polygon(Point(450,50), Point(475,25), Point(500,50))
+        tri.draw(win)
+    def drawBody(self, win):
+        body = Rectangle(Point(400,250),Point(550,525))
         body.draw(win)
-        eng1 = Rectangle(Point(400, 525), Point(550, 575))
-        eng2 = Rectangle(Point(425, 575), Point(525, 600))
+    def drawEng(self, win):
+        eng1 = Rectangle(Point(400,525),Point(550,575))
+        eng2 = Rectangle(Point(425,575),Point(525,600))
         eng1.draw(win)
         eng2.draw(win)
+        fire3 = Image(Point(470, 650), "flame.gif")
+        fire3.draw(win)
 
-        MrB = Image("Mr.B!.jpg",Point(475, 200))
-        MrB.draw(win)
+
+
+
 
 
 
 class RocketShip(object):
     def __init__(self,win):
-        cap = Capsule
-        boost = Rocket(575,275,"red",win)
-        boost = Rocket(325,275, "red", win)
+        cap = Capsule(400,250,'red',win)
+        boost = Rocket(575,275,'orange',win)
+        boost = Rocket(325,275, 'orange', win)
+
+
+
+
+
 
